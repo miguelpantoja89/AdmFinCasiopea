99a459535c9665d0fc531b29e2a5a24b76a7bf50
Miguel Angel Pantoja <mgbastechnology@gmail.com>
2019-04-14 21:00:58 +0200

    Paginacion/css/about us

--- infoEmpresas.php
+++ infoEmpresas.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
 include_once ("includes/gestionBD.php");
+include_once ('includes/paginacion_consulta.php');
+/////////////////////////////////////////////////////////////
+if (isset($_SESSION["paginacion"]))
+	$paginacion = $_SESSION["paginacion"];
+	
+$pagina_seleccionada = isset($_GET["PAG_NUM"]) ? (int)$_GET["PAG_NUM"] : (isset($paginacion) ? (int)$paginacion["PAG_NUM"] : 1);
+$pag_tam = isset($_GET["PAG_TAM"]) ? (int)$_GET["PAG_TAM"] : (isset($paginacion) ? (int)$paginacion["PAG_TAM"] : 5);
+
+if ($pagina_seleccionada < 1) 		$pagina_seleccionada = 1;
+if ($pag_tam < 1) 		$pag_tam = 5;
+
+	
+unset($_SESSION["paginacion"]);
 $conexion= crearConexionBD();
 
 $Comando_sql =  "SELECT IdEmpresa,
@@ -8,8 +21,19 @@ Nombre,
 Direccion,
 Telf
 FROM EMPRESAS";
-$stmn = $conexion->query($Comando_sql);
 
+$total_registros = total_consulta($conexion, $Comando_sql);
+$total_paginas = (int)($total_registros / $pag_tam);
+if ($total_registros % $pag_tam > 0)		$total_paginas++;
+
+if ($pagina_seleccionada > $total_paginas)		$pagina_seleccionada = $total_paginas;
+
+	// Generamos los valores de sesión para página e intervalo para volver a ella después de una operación
+$paginacion["PAG_NUM"] = $pagina_seleccionada;
+$paginacion["PAG_TAM"] = $pag_tam;
+$_SESSION["paginacion"] = $paginacion;
+
+$filas = consulta_paginada($conexion, $Comando_sql, $pagina_seleccionada, $pag_tam);
 
 ?>
 
@@ -25,29 +49,7 @@ $stmn = $conexion->query($Comando_sql);
     <title>Empresas</title>
 </head>
 <body>
-    <style>
-    table,td,tr,th{
-    border: 1px solid black;
-    border-collapse: collapse;
-    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-
-   }
-    table{
-      width:100%;
-      
-      
-      
-  }
-th{
-   color: white;
-   background-color: #565656;
-   opacity:0.6;
-  }
-th,tr, td {
-       padding: 15px;
-       text-align: left;
-}
-    </style>    
+    
     <?php include('cabecera.php') ?>
     <?php include('navegacion.php') ?>
     <main>
@@ -55,13 +57,32 @@ th,tr, td {
        <section>
         <article class="inp">
             <div class="contenedor">
+            <div class="caja3">
+            <form method="get" action="infoEmpresas.php">
+
+                <input id="PAG_NUM" name="PAG_NUM" type="hidden" value="<?php echo $pagina_seleccionada?>"/>
+
+                        Mostrando
+
+                <input id="PAG_TAM" name="PAG_TAM" type="number"
+
+                min="1" max="<?php echo $total_registros; ?>"
+
+                 value="<?php echo $pag_tam?>" autofocus="autofocus" />
+
+            entradas de <?php echo $total_registros?>
+
+                <input type="submit" value="Cambiar">
+
+            </form>
+            </div>
             <table>
             <tr>
             <th>Nombre</th>
             <th>Direccion</th>
             <th>Teléfono</th>
             </tr>
-            <?php foreach ($stmn as $Fila) {
+            <?php foreach ($filas as $Fila) {
 				
                     ?>
                  <tr>	
@@ -72,8 +93,26 @@ th,tr, td {
                    <?php } ?>
                    
             </table>
-          
-                </div>     
+            <div class="caja2" >
+
+<?php
+
+
+    for( $pagina = 1; $pagina <= $total_paginas; $pagina++ )
+
+        if ( $pagina == $pagina_seleccionada) { 	?>
+
+           <span class="current"> <?php echo $pagina; ?></span> 
+
+<?php }	else { ?>
+
+            <a href="infoEmpresas.php?PAG_NUM=<?php echo $pagina; ?>&PAG_TAM=<?php echo $pag_tam; ?>"><?php echo $pagina; ?></a>
+
+<?php } ?>
+
+</div> 
+                </div>    
+                
          </article>
         </section>
         
--- inicio.php
+++ inicio.php
@@ -1,14 +1,37 @@
 <?php
 session_start();
 include_once ('includes/gestionBD.php');
+include_once ('includes/paginacion_consulta.php');
+/////////////////////////////////////////////////////////////
+if (isset($_SESSION["paginacion"]))
+	$paginacion = $_SESSION["paginacion"];
+	
+$pagina_seleccionada = isset($_GET["PAG_NUM"]) ? (int)$_GET["PAG_NUM"] : (isset($paginacion) ? (int)$paginacion["PAG_NUM"] : 1);
+$pag_tam = isset($_GET["PAG_TAM"]) ? (int)$_GET["PAG_TAM"] : (isset($paginacion) ? (int)$paginacion["PAG_TAM"] : 5);
 
+if ($pagina_seleccionada < 1) 		$pagina_seleccionada = 1;
+if ($pag_tam < 1) 		$pag_tam = 5;
 
-
+	
+unset($_SESSION["paginacion"]);
+///////////////////////////////////////////////////////////////////777
 $conexion= crearConexionBD();
-
-$Comando_sql =  "SELECT IdC, DIRECCION FROM COMUNIDADES";
+/////////////////////////////////////////////////////////////!!!!!!
+$Comando_sql =  'SELECT IdC, DIRECCION FROM COMUNIDADES';
 $Resultado=$conexion->query($Comando_sql);
+//////////////////////////////////////////////////////////////////////
+$total_registros = total_consulta($conexion, $Comando_sql);
+$total_paginas = (int)($total_registros / $pag_tam);
+if ($total_registros % $pag_tam > 0)		$total_paginas++;
 
+if ($pagina_seleccionada > $total_paginas)		$pagina_seleccionada = $total_paginas;
+
+	// Generamos los valores de sesión para página e intervalo para volver a ella después de una operación
+$paginacion["PAG_NUM"] = $pagina_seleccionada;
+$paginacion["PAG_TAM"] = $pag_tam;
+$_SESSION["paginacion"] = $paginacion;
+
+$filas = consulta_paginada($conexion, $Comando_sql, $pagina_seleccionada, $pag_tam);
 ?>
 
 
@@ -27,11 +50,31 @@ $Resultado=$conexion->query($Comando_sql);
     <?php include_once ('navegacion.php') ?>
   
     <main>
-    
+    <article class="contenedor">
+<div class= "caja3">
+<form method="get" action="inicio.php">
+
+			<input id="PAG_NUM" name="PAG_NUM" type="hidden" value="<?php echo $pagina_seleccionada?>"/>
+
+			Mostrando
+
+			<input id="PAG_TAM" name="PAG_TAM" type="number"
+
+				min="1" max="<?php echo $total_registros; ?>"
+
+				value="<?php echo $pag_tam?>" autofocus="autofocus" />
+
+			entradas de <?php echo $total_registros?>
+
+			<input type="submit" value="Cambiar">
+
+		</form>
+</div>
+
        <section>
-        <article class="contenedor">
+        
            
-             <?php foreach ($Resultado as $Fila) {
+             <?php foreach ($filas as $Fila) {
 					
 				?>	
 			
@@ -78,6 +121,24 @@ $Resultado=$conexion->query($Comando_sql);
            
                     
         </div>
+        <div class="caja2" >
+
+<?php
+
+
+    for( $pagina = 1; $pagina <= $total_paginas; $pagina++ )
+
+        if ( $pagina == $pagina_seleccionada) { 	?>
+
+           <span class="current"> <?php echo $pagina; ?></span> 
+
+<?php }	else { ?>
+            
+            <a href="inicio.php?PAG_NUM=<?php echo $pagina; ?>&PAG_TAM=<?php echo $pag_tam; ?>"> <?php echo $pagina; ?></a>
+
+<?php } ?>
+
+</div>
         <?php
         if(isset($_SESSION["excepcion"])){
          
